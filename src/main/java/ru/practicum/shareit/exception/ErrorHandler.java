b6edd2f2a59dbcff72ab9e9575514d92dafaea30
AnshakovNikita <kubishe1488@yandex.ru>
2022-10-07 10:51:02 +0300

    исправил замечания, но возможно не правильно их понял)

--- src/main/java/ru/practicum/shareit/exception/ErrorHandler.java
+++ src/main/java/ru/practicum/shareit/exception/ErrorHandler.java
@@ -7,6 +7,7 @@ import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 import javax.validation.ConstraintViolationException;
+import java.util.Map;
 
 @RestControllerAdvice
 public class ErrorHandler {
@@ -14,19 +15,26 @@ public class ErrorHandler {
     @ExceptionHandler({MethodArgumentNotValidException.class,
             ConstraintViolationException.class})
     @ResponseStatus(HttpStatus.BAD_REQUEST)
-    public ErrorResponse validationExceptionHandler(final RuntimeException e) {
-        return new ErrorResponse(e.getMessage());
+    public Map<String, String> validationExceptionHandler(final RuntimeException e) {
+        return Map.of("Error: ", e.getMessage());
     }
 
     @ExceptionHandler(EntityNotFoundException.class)
     @ResponseStatus(HttpStatus.NOT_FOUND)
-    public ErrorResponse notFoundExceptionHandler(final EntityNotFoundException e) {
-        return new ErrorResponse(e.getMessage());
+    public Map<String, String> notFoundExceptionHandler(final EntityNotFoundException e) {
+        return Map.of("Error: ", e.getMessage());
     }
 
     @ExceptionHandler(EntityAlreadyExistException.class)
     @ResponseStatus(HttpStatus.CONFLICT)
-    public ErrorResponse conflictExceptionHandler(final EntityAlreadyExistException e) {
-        return new ErrorResponse(e.getMessage());
+    public Map<String, String> conflictExceptionHandler(final EntityAlreadyExistException e) {
+        return Map.of("Error: ", e.getMessage());
     }
+
+    @ExceptionHandler
+    @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
+    public Map<String, String> handleThrowable(final Throwable e) {
+        return Map.of("Произошла непредвиденная ошибка.", e.getMessage());
+    }
+
 }
